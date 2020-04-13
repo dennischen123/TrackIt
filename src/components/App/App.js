6b10e50e38215b958b4d5ec3a98acdaf06e19084
dennischen123 <dennischen123@gmail.com>
2020-04-13 16:57:45 -0700

    user and warranty api added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './App.css';
+import warrantyAPI from './../../api/warrantyAPI';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    let uid = "5e94b4dd52fc3c5ec96c278a"
+    warrantyAPI.index(uid)
+      .then(res => console.log(res));  
+  }
+
   render() {
     return (
       <div className="APP">
